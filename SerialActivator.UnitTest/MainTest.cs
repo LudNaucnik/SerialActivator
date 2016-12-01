5de393d2e6ba934259978dd0a5756ee8ebee68b9
Ljube <Ljube@LJUBE-PC>
2016-12-01 10:55:46 +0100

    add test

---
+++ SerialActivator.UnitTest/MainTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerialOperations;
+
+namespace SerialActivator.UnitTest
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Operations SOperations = new Operations();
+            String Key = "awdawd";
+            Assert.IsFalse(SOperations.VerifyCode(Key));
+        }
+    }
+}
